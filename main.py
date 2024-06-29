375dde593f06bbbf7d6150e3a7b24a6108f4fafb
8810flags <t.flag.mfmf@gmail.com>
2024-06-29 14:01:26 +0000

    add main.py

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchvision import transforms
+from torch.optim.lr_scheduler import ReduceLROnPlateau, OneCycleLR
 
 
-def set_seed(seed):
+ def set_seed(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
@@ -310,7 +311,7 @@ class VQAModel(nn.Module):
 
 
 # 4. 学習の実装
-def train(model, dataloader, optimizer, criterion, device):
+def train(model, dataloader, optimizer, criterion, device,scheduler):
     model.train()
 
     total_loss = 0
@@ -325,9 +326,11 @@ def train(model, dataloader, optimizer, criterion, device):
         pred = model(image, question)
         loss = criterion(pred, mode_answer.squeeze())
 
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        scheduler.step()
 
         total_loss += loss.item()
         total_acc += VQA_criterion(pred.argmax(1), answers)  # VQA accuracy
@@ -366,10 +369,12 @@ def main():
     # dataloader / model
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
+        transforms.RandomCrop(32, padding=(4, 4, 4, 4), padding_mode='constant'), 
+        transforms.RandomHorizontalFlip(p=0.5),
         transforms.ToTensor()
     ])
-    train_dataset = VQADataset(df_path="./data/train.json", image_dir="./data/train", transform=transform)
-    test_dataset = VQADataset(df_path="./data/valid.json", image_dir="./data/valid", transform=transform, answer=False)
+    train_dataset = VQADataset(df_path="/kaggle/input/vqadata/vqa/train.json", image_dir="/kaggle/input/vqadata/vqa/train/train", transform=transform)
+    test_dataset = VQADataset(df_path="/kaggle/input/vqadata/vqa/valid.json", image_dir="/kaggle/input/vqadata/vqa/valid/valid", transform=transform, answer=False)
     test_dataset.update_dict(train_dataset)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
@@ -377,14 +382,16 @@ def main():
 
     model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
 
-    # optimizer / criterion
-    num_epoch = 20
+     # optimizer / criterion
+    num_epoch = 50
+    ini_lr=0.001
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=ini_lr, weight_decay=1e-5)
+    scheduler = OneCycleLR(optimizer, max_lr=ini_lr, steps_per_epoch=len(train_loader), epochs=num_epoch, pct_start=0.2)
 
     # train model
     for epoch in range(num_epoch):
-        train_loss, train_acc, train_simple_acc, train_time = train(model, train_loader, optimizer, criterion, device)
+        train_loss, train_acc, train_simple_acc, train_time = train(model, train_loader, optimizer, criterion, device,scheduler)
         print(f"【{epoch + 1}/{num_epoch}】\n"
               f"train time: {train_time:.2f} [s]\n"
               f"train loss: {train_loss:.4f}\n"
@@ -403,7 +410,7 @@ def main():
     submission = [train_dataset.idx2answer[id] for id in submission]
     submission = np.array(submission)
     torch.save(model.state_dict(), "model.pth")
-    np.save("submission.npy", submission)
+    np.save("/kaggle/working/submission2.npy", submission)
 
 if __name__ == "__main__":
     main()
